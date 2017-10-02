6e74de69ad5466e4b43ea0ba20c468c9a3b944dd
sainitripti <triptisaini123@gmail.com>
2017-10-02 06:47:51 +0000

    Add files via upload

---
+++ reversearr.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+void reverse(int ar[],int first,int last);
+void print(int ar[],int n);
+int main()
+{
+ int ar[]={1,2,3,4,5,6,7,8};
+ int n=sizeof(ar)/sizeof(ar[0]);
+ print(ar,n);
+ reverse(ar,0,n-1);
+ print(ar,n);
+ return 0;
+}
+/*
+void reverse(int ar[],int n)
+{
+ int i=0,j=n-1,temp;
+ while(i<j)
+ {
+  temp=ar[i];
+  ar[i]=ar[j];
+  ar[j]=temp;
+  i++;
+  j--;
+ }
+}
+*/
+
+void reverse(int ar[],int first,int last)
+{
+ int temp;
+ if(first>=last)
+  return;
+ temp=ar[first];
+ ar[first]=ar[last];
+ ar[last]=temp;
+ reverse(ar,first+1,last-1);
+}
+
+void print(int ar[],int n)
+{
+ cout<<"\n";
+ for(int i=0;i<n;i++)
+  cout<<ar[i]<<"  ";
+ cout<<"\n";
+}
